89a49709a93e4b14aa1da6f8bb804df5d582df4f
Fabiola del Castillo <114948063+fabi3086@users.noreply.github.com>
2023-03-24 14:22:58 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const homeSection = document.getElementById("home")
+homeSection.addEventListener("click", function(){
+    window.location.href = "/index.html";
+})
+
+const aboutMeSection = document.getElementById("about-me")
+aboutMeSection.addEventListener("click", function(){
+    window.location.href = "/about-me/index.html";
+})
+
+const heroSection = document.getElementById("hero-section")
+if(heroSection){
+    heroSection.addEventListener("click", function(){
+        window.location.href = "/post/index.html";
+    })   
+}
+
+const article1Section = document.getElementById("article-1")
+if(article1Section){
+    article1Section.addEventListener("click", function(){
+    window.location.href = "/post/index.html";
+    })
+}
+
+const article2Section = document.getElementById("article-2")
+if(article2Section){
+    article2Section.addEventListener("click", function(){
+    window.location.href = "/post/index.html";
+    })
+}
+
+const article3Section = document.getElementById("article-3")
+if(article3Section){
+    article3Section.addEventListener("click", function(){
+    window.location.href = "/post/index.html";
+    })
+}
+
+const article4Section = document.getElementById("article-4")
+if(article4Section){
+    article4Section.addEventListener("click", function(){
+        window.location.href = "/post/index.html";
+    })
+}
+
+const article5Section = document.getElementById("article-5")
+if(article5Section){
+    article5Section.addEventListener("click", function(){
+        window.location.href = "/post/index.html";
+    })
+}
+
+const article6Section = document.getElementById("article-6")
+if(article6Section){
+    article6Section.addEventListener("click", function(){
+        window.location.href = "/post/index.html";
+    })
+}
